a6f3e28d3ce5c543abc4e62a89ce0f83fd9ff2fe
Basma Ashour <basmaashouur@gmail.com>
2022-07-22 09:46:38 -0400

    init commit

---
+++ src/main/java/BIC/bic/BicApplication.java
@@ -0,0 +1,13 @@
+package BIC.bic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BIC/bic/BicApplicationTests.java
@@ -0,0 +1,13 @@
+package BIC.bic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
